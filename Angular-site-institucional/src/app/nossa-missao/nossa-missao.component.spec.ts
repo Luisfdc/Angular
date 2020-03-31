226ee44eda9847db98e4f66488f1037c948cd05c
Luis Fernando Dias Campos Santos <luisfd.campos@gmail.com>
2020-03-31 08:45:54 -0300

    Criação de components e add bootstrap

---
+++ Angular-site-institucional/src/app/nossa-missao/nossa-missao.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { NossaMissaoComponent } from './nossa-missao.component';
+
+describe('NossaMissaoComponent', () => {
+  let component: NossaMissaoComponent;
+  let fixture: ComponentFixture<NossaMissaoComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ NossaMissaoComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(NossaMissaoComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
